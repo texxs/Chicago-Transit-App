4b0e9c9a2bb23639cdb7ef7b6d9db3a81312bca4
Texx Smith <texxsmith@yahoo.com>
2019-09-26 09:59:09 -0400

    Initial comitt

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Chicago Transit Authority - Train Schedules and Status</title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link href="css/styles.css" rel="stylesheet" type="text/css">
+  
+  <script src="js/stations.js"></script>
+  <script src="js/moment.js"></script>
+  <script src="js/moment-timezone.js"></script>
+  <script src="chicago.js"></script>
+</head>
+  
+  
+<body>
+  
+  <div id="container">
+    
+    <div id="stationSched"></div>
+    
+    <div id="trainInfo"></div>
+    
+  </div>
+  
+</body>
+</html>
